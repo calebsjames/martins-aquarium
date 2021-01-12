18671ef4ef9fe257f7305cdc65cc870339b146f8
Caleb James <calebsjames@gmail.com>
2021-01-12 14:53:38 -0600

    added scripts folder and FishDataProvider.js file

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,10 @@
+const fishCollection = [
+    {
+        name: "Nemo",
+        species: "Clown Fish",
+        Location: "Great Barrier Reef",
+        food: "Yes, you can eat Nemo",
+        length: "3 inches",
+
+    }
+]
