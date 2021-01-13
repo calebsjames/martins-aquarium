c9b2007e6a70944df5bccf0673eaeb19fe48a26e
Caleb James <calebsjames@gmail.com>
2021-01-13 09:31:41 -0600

    chapter 9

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -8,3 +8,7 @@ const fishCollection = [
 
     }
 ]
+
+export const useFish = () => {
+    return fishCollection.slice()
+}
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+import { use fish } from './FishDataProvider.js'
+
+const allTheFish = useFish()
+
+for (const fish of allTheFish) {
+    console.log(fish)
+}
